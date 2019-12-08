c72237d18eb48c7620c021101de6b53375f2ac30
Simon Emanuel Schmid <simon@schmid.io>
2019-12-08 15:57:25 +0100

    Basic styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,29 @@
-function Home() {
-  return <div>Welcome to Next.js!</div>;
-}
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div className="root">
+      <Head>
+        <title>Schlawiner</title>
+      </Head>
+      <style jsx global>{`
+        html,
+        body {
+          width: 100%;
+          min-height: 100%;
+          background-color: #fe0000;
+          background-image: url('/bg.jpg');
+          background-position: center center;
+          background-repeat: no-repeat;
+          background-size: 50%;
+        }
+      `}</style>
+      <style jsx>{`
+        .root {
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default Home;
